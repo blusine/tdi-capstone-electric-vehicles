449991e7349aa5abf5533a74a7defc21c9a5d51d
blusine <blusine@hotmail.com>
2023-07-04 17:54:54 -0500

    sidebar2

--- streamlit_app.py
+++ streamlit_app.py
@@ -94,7 +94,8 @@ city_coordinates = df_cities['geoloc']
 
 city_choices = list(df_cities["city_state"])
 city_choices.insert(0, "Select a City")
-vehicle_choices = list(zip(df_vehicles['make'], df_vehicles['model']))
+#vehicle_choices = list(zip(df_vehicles['make'], df_vehicles['model']))
+vehicle_choices = list(f"{df_vehicles['make']} , {df_vehicles['model']}")
 
 with st.sidebar.form(key="my_form"):
     selectbox_city = st.selectbox("Choose a City", city_choices)
