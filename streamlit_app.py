f5ca73b0f8fab57dc4228dcd6b6debaf43c39d84
blusine <blusine@hotmail.com>
2023-07-05 18:27:59 -0500

    c

--- streamlit_app.py
+++ streamlit_app.py
@@ -304,9 +304,11 @@ folium_static(map_obj)
 # Draw a chart with monthly estimated costs
 if selected_city and selected_vehicle:
     column_names = ['Period', 'Estimated Cost']
-    df = pd.DataFrame(monthly_dollars)
-    st.write(
-    f"{df.columns.tolist()}"
-    )    
+    df = pd.DataFrame(monthly_dollars)  
     df.reset_index(level=0, inplace=True)
-    df.rename(columns = {columns[0]: 'Period', columns[1]: 'Estimated Cost'}, inplace = True)
+    
+    st.write(
+    f"{df}"
+    ) 
+    
+    #df.rename(columns = {columns[0]: 'Period', columns[1]: 'Estimated Cost'}, inplace = True)
