072d1f0918cbc1cc45cc3b6228e15feed36f4631
blusine <blusine@hotmail.com>
2023-07-04 20:22:38 -0500

    cc

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,6 @@ with col2:
      
 st.markdown(
 """
-Directions:
 - Select the closest city where you intend to drive the vehicle
 - Select the vehicle you are interested in from the drop down list
 - Select the expected annual miles you intend to drive
