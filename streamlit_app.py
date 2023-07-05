5868ed6140c63115fdf70122d224cab864382203
blusine <blusine@hotmail.com>
2023-07-05 02:22:54 -0500

    mm

--- streamlit_app.py
+++ streamlit_app.py
@@ -91,16 +91,20 @@ def load_data(filename):
         
     return data
 
-# read the data
-city_data = load_data('data/cities_geocoded.json')
-vehicle_data = load_data('data/electric_vehicles.json')
+# read the data somehow the json data did not get parsed correctly, so I reading csv and converting to json again
+city_data = load_data('data/cities_geocoded.csv')
+city_data = city_data.apply(lambda x: json.dumps(x.to_dict(), ensure_ascii=False), axis=1)
+city_data = city_data.to_list()
+
+vehicle_data = load_data('data/electric_vehicles.csv')
+vehicle_data = vehicle_data.apply(lambda x: json.dumps(x.to_dict(), ensure_ascii=False), axis=1)
+vehicle_data = vehicle_data.to_list()
 
 #Initialize data
 #city_coordinates = list(city['geoloc'] for city in city_data)
 for city in city_data:
-    city = json.loads(city)
     city["cost"] = " "
-    st.write(f"{type(city)}")
+    #st.write(f"{type(city)}")
 
 #city_costs = [{ciy: " "} for city in city_data['city']]
                      
